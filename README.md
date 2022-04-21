fc866542b0e24779911e78781ba6d65aa43c6e5e
sugiyama ryota <guqeeky6va8@gmail.com>
2022-04-21 10:05:42 +0900

    add chapter-5

---
+++ README.md
@@ -0,0 +1,8 @@
+オーバーロードとは
+https://typescriptbook.jp/reference/functions/overload-functions
+
+ミックインとは
+https://www.typescriptlang.org/docs/handbook/mixins.html
+
+デコレーターとは
+https://www.typescriptlang.org/docs/handbook/decorators.html
---
+++ chapter-5/src/withEZDebug.ts
@@ -0,0 +1,31 @@
+type ClassConstructor<T> = new(...args: any[]) => T
+
+function withEZDebug<C extends ClassConstructor<{
+    getDebugValue(): object
+}>>(Class: C){
+    return class extends Class {
+        debug() {
+            let Name = super.constructor.name;
+            let value = this.getDebugValue();
+            return Name + '(' + JSON.stringify(value) + ')'
+        }
+    }
+}
+
+class HardToDebugUser {
+    constructor(
+        private id: number,
+        private firstName: string,
+        private lastName: string
+    ){}
+    getDebugValue() {
+        return {
+            id: this.id,
+            name: this.firstName + ' ' + this.lastName
+        }
+    }
+}
+
+let User = withEZDebug(HardToDebugUser)
+let user = new User(3, 'Emma', 'Gluzman')
+user.debug();
